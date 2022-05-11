e0d1130a2bc2f2276630f2ca1195b4b5521af205
joshua.andujar <joshua.andujar@bstock.com>
2022-05-11 18:59:43 -0400

    getting the api key into my own constant although not strictly necessary since we can access BuildConfig anywhere

--- app/src/main/java/com/example/mowpro/Constants.kt
+++ app/src/main/java/com/example/mowpro/Constants.kt
@@ -1,8 +1,6 @@
 package com.example.mowpro
 
-import android.content.res.Resources
 
-private val API_KEY = Resources.getSystem().getString(R.string.api_key)
-
-const val CURRENT_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?"
-val CURRENT_WEATHER_TEST_PARAMS = "appid=$API_KEY&lat=28.432590&lon=-81.238850&units=imperial"
+const val CURRENT_WEATHER_API_URL = "https://api.openweathermap.org/"
+const val CURRENT_WEATHER_API_PATH = "data/2.5/weather"
+const val OPEN_WEATHER_API_KEY = BuildConfig.api_key
