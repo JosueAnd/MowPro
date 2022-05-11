5eebcda218ea2c5c9a5de82acebb8bcb1c926042
joshua.andujar <joshua.andujar@bstock.com>
2022-05-11 19:12:49 -0400

    adding fields to the CurrentWeatherData data class

--- app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
+++ app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
@@ -2,4 +2,6 @@ package com.example.mowpro.network
 
 import com.squareup.moshi.Json
 
-data class CurrentWeatherData(val id: Int)
+data class CurrentWeatherData(
+        @Json(name = "weather") val weatherDescriptor: List<Map<String, Any>>,
+        @Json(name = "main") val weatherTemperatures: Map<String, Any>)
