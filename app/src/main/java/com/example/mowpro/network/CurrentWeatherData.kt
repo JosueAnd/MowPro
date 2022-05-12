d17d8fefa808fca17687303a5808c1075238dfb5
joshua.andujar <joshua.andujar@bstock.com>
2022-05-11 23:30:45 -0400

    getting specific values from the JSON and assigning to class variables

--- app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
+++ app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
@@ -1,7 +1,30 @@
 package com.example.mowpro.network
 
 import com.squareup.moshi.Json
+import kotlin.math.roundToInt
 
-class CurrentWeatherData(
-        @Json(name = "weather") val weatherDescriptor: List<Map<String, Any>>,
-        @Json(name = "main") val weatherTemperatures: Map<String, Any>)
+data class CurrentWeatherData(
+        @Json(name = "weather") private val weatherDescriptors: List<Map<String, Any>>,
+        @Json(name = "main") private val weatherTemperatures: Map<String, Any>) {
+
+    val weatherDescriptor: String = determineWeatherDescriptor()
+    val weatherCurrentTemp: Int = determineWeatherTemp("feels_like")
+
+    private fun determineWeatherDescriptor(): String {
+        if (weatherDescriptors.isEmpty())
+            return "No Description Available"
+
+        val descriptors = weatherDescriptors.first()
+        if (descriptors.containsKey("main"))
+            return descriptors.get("main").toString()
+        return "No Description Available"
+    }
+
+    private fun determineWeatherTemp(key: String): Int {
+        if (weatherTemperatures.isEmpty())
+            return 0
+        if (weatherTemperatures.get(key) is Number)
+            return (weatherTemperatures.get(key) as Number).toDouble().roundToInt()
+        return 0
+    }
+}
