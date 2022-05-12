24bb61fc266dea36a5543094d27f36ea8e575fbc
joshua.andujar <joshua.andujar@bstock.com>
2022-05-11 21:08:46 -0400

    moving away from data class, may come back to this

--- app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
+++ app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
@@ -2,6 +2,6 @@ package com.example.mowpro.network
 
 import com.squareup.moshi.Json
 
-data class CurrentWeatherData(
+class CurrentWeatherData(
         @Json(name = "weather") val weatherDescriptor: List<Map<String, Any>>,
         @Json(name = "main") val weatherTemperatures: Map<String, Any>)
