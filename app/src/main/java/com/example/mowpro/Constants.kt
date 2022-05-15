c3e797ce50e48b9d315c71ff6d7dcd9b09acb202
joshua.andujar <joshua.andujar@bstock.com>
2022-05-15 00:21:35 -0400

    adding degree fahrenheit symbol constant

--- app/src/main/java/com/example/mowpro/Constants.kt
+++ app/src/main/java/com/example/mowpro/Constants.kt
@@ -1,6 +1,10 @@
 package com.example.mowpro
 
 
+// Weather API Constants
 const val CURRENT_WEATHER_API_URL = "https://api.openweathermap.org/"
 const val CURRENT_WEATHER_API_PATH = "data/2.5/weather"
 const val OPEN_WEATHER_API_KEY = BuildConfig.api_key
+
+// Temperature Degrees Fahrenheit Symbol
+const val DEGREES_FAHRENHEIT = "\u2109"
