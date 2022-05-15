775ca80de0b788a4610d7c948035836e71f52e31
joshua.andujar <joshua.andujar@bstock.com>
2022-05-15 08:38:34 -0400

    utilizing language features to eliminate if checks

--- app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
+++ app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
@@ -1,30 +1,27 @@
 package com.example.mowpro.network
 
 import com.squareup.moshi.Json
-import kotlin.math.roundToInt
 
 data class CurrentWeatherData(
         @Json(name = "weather") private val weatherDescriptors: List<Map<String, Any>>,
         @Json(name = "main") private val weatherTemperatures: Map<String, Any>) {
 
-    val weatherDescriptor: String = determineWeatherDescriptor()
-    val weatherCurrentTemp: Int = determineWeatherTemp("feels_like")
+    val weatherDescriptor: String? = determineWeatherDescriptor("main")
+    val weatherCurrentTemp: Double? = determineWeatherTemp("feels_like")
+    val weatherIconName: String? = determineWeatherIcon()
 
-    private fun determineWeatherDescriptor(): String {
-        if (weatherDescriptors.isEmpty())
-            return "No Description Available"
-
-        val descriptors = weatherDescriptors.first()
-        if (descriptors.containsKey("main"))
-            return descriptors.get("main").toString()
-        return "No Description Available"
+    private fun determineWeatherDescriptor(key:String): String? {
+        val descriptors = weatherDescriptors.firstOrNull()
+        return descriptors?.get(key)?.toString()
     }
 
-    private fun determineWeatherTemp(key: String): Int {
-        if (weatherTemperatures.isEmpty())
-            return 0
-        if (weatherTemperatures.get(key) is Number)
-            return (weatherTemperatures.get(key) as Number).toDouble().roundToInt()
-        return 0
+    private fun determineWeatherIcon(): String? {
+        val iconName = determineWeatherDescriptor("icon")
+        return if (iconName != "") iconName else null
+    }
+
+    private fun determineWeatherTemp(key: String): Double? {
+        val temp = weatherTemperatures.get(key)
+        return if (temp is Number) temp.toDouble() else null
     }
 }
