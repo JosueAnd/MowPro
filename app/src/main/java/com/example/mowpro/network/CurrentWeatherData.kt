4209f01502ffc96060952f5e65b8de71d4374820
joshua.andujar <joshua.andujar@bstock.com>
2022-05-15 20:34:06 -0400

    revamping some stuff in trying to get livedata to work

--- app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
+++ app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
@@ -2,26 +2,25 @@ package com.example.mowpro.network
 
 import com.squareup.moshi.Json
 
-data class CurrentWeatherData(
-        @Json(name = "weather") private val weatherDescriptors: List<Map<String, Any>>,
-        @Json(name = "main") private val weatherTemperatures: Map<String, Any>) {
+data class CurrentWeatherData(@Json(name = "weather") private val weatherDescriptors: List<Map<String, Any>>,
+                              @Json(name = "main") private val weatherTemperatures: Map<String, Any>) {
 
-    val weatherDescriptor: String? = determineWeatherDescriptor("main")
-    val weatherCurrentTemp: Double? = determineWeatherTemp("feels_like")
-    val weatherIconName: String? = determineWeatherIcon()
+    val weatherDescriptor: String = determineWeatherDescriptor("main")
+    val weatherCurrentTemp: Double = determineWeatherTemp("feels_like")
+    val weatherIconName: String = determineWeatherDescriptor("icon")
 
-    private fun determineWeatherDescriptor(key:String): String? {
+    private fun determineWeatherDescriptor(key:String): String {
         val descriptors = weatherDescriptors.firstOrNull()
-        return descriptors?.get(key)?.toString()
+        return descriptors?.get(key)?.toString() ?: ""
     }
 
-    private fun determineWeatherIcon(): String? {
-        val iconName = determineWeatherDescriptor("icon")
-        return if (iconName != "") iconName else null
-    }
-
-    private fun determineWeatherTemp(key: String): Double? {
-        val temp = weatherTemperatures.get(key)
-        return if (temp is Number) temp.toDouble() else null
+    private fun determineWeatherTemp(key: String): Double {
+        val temp = weatherTemperatures[key] ?: 0.0
+        return if (temp is Number) temp.toDouble() else 0.0
     }
 }
+
+
+val fakeWeatherData = CurrentWeatherData(listOf(mapOf("main" to "Not Available",
+                                                      "icon" to "01d")),
+                                         mapOf("feels_like" to 0.0))
