b20320648a6ea428049aebfe9b9b2c39097455af
joshua.andujar <joshua.andujar@bstock.com>
2022-05-16 22:42:46 -0400

    layout is updated to have degrees F rather than decimal

--- app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
+++ app/src/main/java/com/example/mowpro/network/CurrentWeatherData.kt
@@ -1,26 +1,25 @@
 package com.example.mowpro.network
 
+import android.util.Log
 import com.squareup.moshi.Json
 
-data class CurrentWeatherData(@Json(name = "weather") private val weatherDescriptors: List<Map<String, Any>>,
-                              @Json(name = "main") private val weatherTemperatures: Map<String, Any>) {
+class CurrentWeatherData(@Json(name = "weather") private val weatherDescriptors: List<Map<String, Any>>,
+                         @Json(name = "main") private val weatherTemperatures: Map<String, Any>) {
 
-    val weatherDescriptor: String = determineWeatherDescriptor("main")
-    val weatherCurrentTemp: Double = determineWeatherTemp("temp")
-    val weatherIconName: String = determineWeatherDescriptor("icon")
+//    val weatherDescriptor: String = determineWeatherDescriptor("main")
+//    private val weatherCurrentTemp: Double = determineWeatherTemp(weatherTemperatures, "temp")
+//    val weatherIconName: String = determineWeatherDescriptor("icon")
 
-    private fun determineWeatherDescriptor(key:String): String {
-        val descriptors = weatherDescriptors.firstOrNull()
-        return descriptors?.get(key)?.toString() ?: ""
-    }
+//    private val _weatherCurrentTemp = setTheCurrentTemp()
+    val weatherCurrentTempInt: Double  = determineWeatherTemp("temp")
+
+//    private fun determineWeatherDescriptor(from: List<Map<String, Any>>, key: String): String {
+//        val descriptors = from.firstOrNull()
+//        return descriptors?.get(key)?.toString() ?: ""
+//    }
 
     private fun determineWeatherTemp(key: String): Double {
         val temp = weatherTemperatures[key] ?: 0.0
         return if (temp is Number) temp.toDouble() else 0.0
     }
 }
-
-
-val fakeWeatherData = CurrentWeatherData(listOf(mapOf("main" to "Not Available",
-                                                      "icon" to "01d")),
-                                         mapOf("feels_like" to 0.0))
--- app/src/main/java/com/example/mowpro/viewmodels/WeatherCardViewModel.kt
+++ app/src/main/java/com/example/mowpro/viewmodels/WeatherCardViewModel.kt
@@ -2,44 +2,37 @@ package com.example.mowpro.viewmodels
 
 import android.util.Log
 import androidx.lifecycle.*
+import com.example.mowpro.DEGREES_FAHRENHEIT
 import com.example.mowpro.network.CurrentWeatherApi
 import com.example.mowpro.network.CurrentWeatherApiStatus
 import kotlinx.coroutines.launch
 import com.example.mowpro.network.CurrentWeatherData
-import com.example.mowpro.network.fakeWeatherData
 import java.lang.IllegalArgumentException
 
 class WeatherCardViewModel: ViewModel() {
 
     private val logTag = "WeatherCardVM"
 
-    private val _currentWeatherData = MutableLiveData<CurrentWeatherData>()
-    val currentWeatherData: LiveData<CurrentWeatherData> = _currentWeatherData
-    private val _weatherTemp = MutableLiveData<String>()
-    val weatherTemp: LiveData<String> = _weatherTemp
-
     private val _status = MutableLiveData<CurrentWeatherApiStatus>()
-//    val status: LiveData<CurrentWeatherApiStatus> = _status
 
-    init { getTheCurrentWeatherData() }
+    private var currentWeatherData = MutableLiveData<CurrentWeatherData>()
+    var weatherTemp: LiveData<String> = Transformations.map(currentWeatherData) { "${it.weatherCurrentTempInt.toInt()}" }
+    var weatherDegreesF: LiveData<String> = Transformations.map(currentWeatherData) { DEGREES_FAHRENHEIT }
 
-    private fun getTheCurrentWeatherData() {
+    init {
         viewModelScope.launch {
             _status.value = CurrentWeatherApiStatus.LOADING
             try {
-                _currentWeatherData.value = CurrentWeatherApi.retrofitService
+                currentWeatherData.value = CurrentWeatherApi.retrofitService
                     .getCurrentWeatherData(latitude = "25",
                                            longitude = "25")
                 _status.value = CurrentWeatherApiStatus.DONE
             } catch (e: Exception) {
-                Log.d(logTag, "Exception Caught: $e")
+                Log.d(logTag, "Exception Caught in WeatherCardViewModel: $e")
                 _status.value = CurrentWeatherApiStatus.ERROR
-                _currentWeatherData.value = fakeWeatherData
+                currentWeatherData = MutableLiveData()
             }
-            Log.d(logTag, "viewModelScope.launch reached end")
-            Log.d(logTag, currentWeatherData.toString())
         }
-        Log.d(logTag, "Exited viewModelScope.launch")
     }
 }
 
