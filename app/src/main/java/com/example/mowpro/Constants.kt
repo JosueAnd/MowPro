33e666792310e71bb68fed830ef5bb41301aec9f
joshua.andujar <joshua.andujar@bstock.com>
2022-06-19 09:37:10 -0400

    renaming the icon url constant

--- app/src/main/java/com/example/mowpro/Constants.kt
+++ app/src/main/java/com/example/mowpro/Constants.kt
@@ -7,7 +7,7 @@ const val CURRENT_WEATHER_API_PATH = "data/2.5/weather"
 const val OPEN_WEATHER_API_KEY = BuildConfig.api_key
 
 // Weather API Icon Constants
-const val WEATHER_ICON_API_URL = "http://openweathermap.org/img/wn/"
+const val WEATHER_ICON_URL = "http://openweathermap.org/img/wn/%s@2x.png"
 
 // Temperature Degrees Fahrenheit Symbol
 const val DEGREES_FAHRENHEIT = "\u2109"
