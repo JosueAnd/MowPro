ab9ad53a497a2ebb0d7409f8d5cd23b4805be88c
joshua.andujar <joshua.andujar@bstock.com>
2022-06-27 22:30:19 -0400

    everything is observed to allow for full livedata utilization. When is inside the observe

--- app/src/main/java/com/example/mowpro/controllers/WeatherCard.kt
+++ app/src/main/java/com/example/mowpro/controllers/WeatherCard.kt
@@ -12,26 +12,27 @@ fun fillWeatherLayout (activity: AppCompatActivity,
                       binding: WeatherLayoutBinding,
                       model: WeatherCardViewModel) {
     model.model.observe(activity) { vm ->
-        when (vm.status.value) {
-            CurrentWeatherApiStatus.LOADING,
-            CurrentWeatherApiStatus.DONE -> {
-                binding.apply {
-                    vm.temperature.observe(activity) { temperature.text = it }
-                    vm.tempScale.observe(activity) { weatherScale.text = it }
-                    vm.description.observe(activity) { weatherDescription.text = it }
-                    vm.iconLink.observe(activity) { weatherIcon.load(it) }
-                    vm.location.observe(activity) { weatherCityState.text = it }
+        binding.apply {
+            vm.temperature.observe(activity) { temperature.text = it }
+            vm.tempScale.observe(activity) { weatherScale.text = it }
+            vm.description.observe(activity) { weatherDescription.text = it }
+            vm.location.observe(activity) { weatherCityState.text = it }
+            vm.iconLink.observe(activity) {
+                when (vm.status.value) {
+                    CurrentWeatherApiStatus.LOADING -> {
+                        weatherIcon.setImageDrawable(activity.getDrawable(R.drawable.weather_pull_in_progress))
+                    }
+                    CurrentWeatherApiStatus.DONE -> {
+                        weatherIcon.load(it)
+                    }
+                    CurrentWeatherApiStatus.ERROR -> {
+                        weatherIcon.setImageDrawable(activity.getDrawable(R.drawable.weather_pull_fail))
+                    }
+                    else -> {
+                        weatherIcon.setImageDrawable(activity.getDrawable(R.drawable.weather_pull_fail))
+                    }
                 }
             }
-            CurrentWeatherApiStatus.ERROR -> {
-                binding.apply {
-                    temperature.text = "-"
-                    weatherScale.text = "-"
-                    weatherDescription.text = "-"
-                    weatherIcon.setImageDrawable(activity.getDrawable(R.drawable.weather_pull_fail))
-                }
-            }
-            else -> {}
         }
     }
 }
